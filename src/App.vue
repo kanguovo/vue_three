14d3863f07e14b890f945015b4a9dd4927102b47
Sky_Ch <2369527762@qq.com>
2026-06-01 20:47:52 +0800

    组合式API_ref和reactive函数

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { reactive, ref } from 'vue';
+const deta =0;
+// const D = reactive(deta);
+// 错误写法是因为reactive只可以接受数组或对象
+const Deta = reactive({deta})
+const a =()=>{
+  Deta.deta++;
+}
+
+const deta_1=ref(0);
+const b =()=>{
+
+  deta_1.value++;
+}
+
+</script>
+<template>
+<div>
+<button @click="a">{{ Deta.deta  }}</button>
+</div>
+<button @click="b">{{ deta_1 }}</button>
+//deta_1.value
+</template>
+
