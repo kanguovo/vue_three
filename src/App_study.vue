5e365867dd2c5a8c4e6995f4947dec6799c0c5fe
Sky_Ch <2369527762@qq.com>
2026-06-02 21:00:17 +0800

    组合式API_生命周期函数

--- src/App_study.vue
+++ src/App_study.vue
@@ -1,60 +1,24 @@
 <script setup>
+import { onMounted } from 'vue';
 
-import { ref, watch } from 'vue';
-
-const ab=ref({a:0,b:0})
-const A=()=>{
-
-//aa.a++;错了应该是aa.value.a++  ref返回的是一个对象（响应式对象）
-ab.value.a++;
-
-}
-const B=()=>{
-
-
-ab.value.b++;
-
-}
-
-
-
-// watch(ab,()=>{
-// console.log("变化了")
-//  },
-// {deep:true }
-
-// ）
-
-//watch 第一个对象不用 .value
-
- //因为这里修改的是对象里面的属性值而非对象本身，所以要用deep
-
-//ab无法指明是哪个属性改变引起的打印，需优化
-
-
-
-watch(()=>ab.value.a,()=>{           //第一个参数 不可以 ab.value.a
-console.log("变化了")
- },
-//不用写deeple了
-)
-
-
-// const temp=ref(ab.value.b )   错误写法 导致 temp.value 和 ab.value.b是两个不同的个体了
-// watch(temp,()=>{        
-// console.log("变化了")
-//  },
-// {deep:true }
-
-// )
-
+//生命周期函数
+//本质上是回调函数，在特定时期下执行
+onMounted(()=>{
+  console.log("执行了1");
+  
+})
+onMounted(()=>{
+  console.log("执行了3");
+  
+})
+onMounted(()=>{
+  console.log("执行了2");
+  
+})
 </script>
-
-
-
 <template>
-  <div>
-    <button @click="A">{{ ab.a }}</button>
-    <button @click="B">{{ ab.b }}</button>
-  </div>
+<div>
+
+</div>
+
 </template>
