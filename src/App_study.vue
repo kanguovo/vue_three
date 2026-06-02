07ca0a5328571df1ad42f7a9efeada69db2b76f6
Sky_Ch <2369527762@qq.com>
2026-06-02 17:41:03 +0800

    组合式API_watch基本使用和立即执行

--- src/App_study.vue
+++ src/App_study.vue
@@ -1,18 +1,56 @@
 <script setup>
-import { computed, ref } from 'vue';
+import { ref, watch } from 'vue';
 
-const a=ref([1,2,3,4,5,6,7,8,9]);
-const ar=computed(()=>{
+const a=ref(0);
+const A=()=>{
+  a.value++;
 
-  return a.value.filter(item=>item>2)
-})
+}
+const b=ref(0);
+const B=()=>{
+  b.value++;
+
+}
+const c=ref(0);
+const C=()=>{
+  b.value++;
+
+}
+
+// watch([a,b],
+// (
+//   [new_a,old_a], 
+//   [new_b,old_b] ;
+// )=> {
+// console.log("回调函数"，[new_a,ole_a];  
+//   [new_b,old_b]; )
+// }
+//注释第三四行错了应该是[new_a,new_b],[old_a,old_b] 并且第四行不要用分号
+ watch([a,b],
+(
+  [new_a,new_b], 
+  [old_a,old_b]
+)=> {
+console.log("回调函数",
+  [new_a,new_b], 
+  [old_a,old_b])
+})//watch函数第二个参数是一个函数，})在最后不要写错，另外注意打印格式，写数组的时候不要写错
+watch(c,(new_c,old_c)=>{
+console.log("回调",new_c,old_c)
+},{
+immediate:true
+}
+
+
+)
 </script>
 
 <template>
   <div>
-           {{ a }}
-  </div>
-  <div>
-    {{ ar }}
+    <button @click="A">{{ a }}</button>
+    <button @click="B">{{ b}}</button>
+    <button @click="C">{{ c}}</button>
+   
   </div>
 </template>
+
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
  import { createApp } from 'vue'
 //和java 里面的引入一样，这里引入核心函数
-import App from './App.vue'
+import App from './App_study.vue'
 //将同一目录下面的app.vue文件导出并赋值为APP
+//改名之后这里要改名字
 createApp(App).mount('#app')
 //creaatApp（根组件）创建示例，是vue3启动的入口函数
 //.mount("#app")是将这里的内容挂载到id为app（index.html中div=app）的节点上
